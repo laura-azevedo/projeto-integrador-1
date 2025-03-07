8b1b0002ee4cdd0b65647745ccec60190f83ade0
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-07 09:31:13 -0300

    chore: initialize Spring project setup

---
+++ projeto-integrador-1-grupo1/src/main/java/com/generation/projetointegrador1/ProjetoIntegrador1Grupo1Application.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegrador1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador1Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador1Grupo1Application.class, args);
+	}
+
+}
---
+++ projeto-integrador-1-grupo1/src/test/java/com/generation/projetointegrador1/ProjetoIntegrador1Grupo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.projetointegrador1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegrador1Grupo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
